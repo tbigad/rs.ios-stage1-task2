81e10763be65aaffdca4b9a372135098a0ace76c
Pavel Nadolski <tbigad@gmail.com>
2020-03-20 18:33:49 +0300

    init: start Task2

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,23 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    [components setDay:[day integerValue]];
+    [components setMonth:[month integerValue]];
+    [components setYear:[year integerValue]];
+    components.calendar = calendar;
+    if(!components.isValidDate) {
+        return @"Такого дня не существует";
+    }
+    NSDate *date = [calendar dateFromComponents:components];
+    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+    [dateFormatter setDateFormat:@"d MMMM, EEEE"];
+    
+    
+    NSString* ret = [dateFormatter stringFromDate:date];
+    return ret;
 }
 
 @end
