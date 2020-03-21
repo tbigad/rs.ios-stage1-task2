9b8dab5472adc89715672f08625876b4014b19ac
Pavel Nadolski <tbigad@gmail.com>
2020-03-21 17:47:12 +0300

    feat: finish FibonacciResolver

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -1,7 +1,35 @@
 #import "FibonacciResolver.h"
 
+@interface FibonacciResolver ()
+- (int) fibonacciAtIndex:(int)n;
+@end
+
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    int current_value = 0;
+    int x = 0;
+    int fib1 = 0;
+    int fib2 = 0;
+    int numberValue = [number intValue];
+
+    while (current_value < numberValue) {
+        x++;
+        fib1 = [self fibonacciAtIndex:x];
+        fib2 = [self fibonacciAtIndex:x+1];
+        current_value = fib1 * fib2;
+    }
+
+    NSNumber* ret1 = [[NSNumber alloc]initWithInt:fib1];
+    NSNumber* ret2 = [[NSNumber alloc]initWithInt:fib2];
+    if (current_value == numberValue) {
+        
+        return @[ret1, ret2, @YES];
+    }
+    return @[ret1, ret2, @NO];
+}
+
+- (int)fibonacciAtIndex:(int)n
+{
+    return ( n<=2 ? 1 : [self fibonacciAtIndex:n-1] + [self fibonacciAtIndex:(n-2)] );
 }
 @end
