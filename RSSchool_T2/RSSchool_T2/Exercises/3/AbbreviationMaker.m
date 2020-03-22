510c319c6ef32d54dcb4edf86ff5e296f01ddada
Pavel Nadolski <tbigad@gmail.com>
2020-03-22 20:02:03 +0300

    feat: finish AbbreviationTests

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,19 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    NSMutableString *uppercaseString = [[a uppercaseString] mutableCopy];
+    int indexB = 0;
+    NSMutableString *resultString = [NSMutableString new];
+    for (int i = 0; i < a.length; i ++) {
+        if (indexB >= b.length) {
+            break;
+        }
+        if([uppercaseString characterAtIndex:i] == [b characterAtIndex:indexB]) {
+            [resultString appendFormat:@"%c",[uppercaseString characterAtIndex:i]];
+            indexB++;
+        }
+    }
+    
+    return [resultString isEqual:b] ? @"YES": @"NO";
 }
 @end
