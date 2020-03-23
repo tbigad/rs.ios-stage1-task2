2eb31baa6cad10eea289435a8ce8aae9eeace272
Pavel Nadolski <tbigad@gmail.com>
2020-03-23 12:05:38 +0300

    feat: simplify task3

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -6,10 +6,8 @@
     NSMutableString *uppercaseString = [[a uppercaseString] mutableCopy];
     int indexB = 0;
     NSMutableString *resultString = [NSMutableString new];
-    for (int i = 0; i < a.length; i ++) {
-        if (indexB >= b.length) {
-            break;
-        }
+    
+    for (int i = 0; (i < a.length) && (indexB < b.length); i ++) {
         if([uppercaseString characterAtIndex:i] == [b characterAtIndex:indexB]) {
             [resultString appendFormat:@"%c",[uppercaseString characterAtIndex:i]];
             indexB++;
